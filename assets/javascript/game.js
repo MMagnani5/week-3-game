1bd5a4ce6d0fa02bb4f45eac25fa427e61267be7
Mike Magnani <mikemagnani5@gmail.com>
2016-09-02 15:10:30 -0400

    added play function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -82,7 +82,80 @@ window.onload = function () {
     }
   }
 
- 
+ // OnClick Function
+   check = function () {
+    list.onclick = function () {
+      var guess = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === guess) {
+          guesses[i].innerHTML = guess;
+          counter += 1;
+        } 
+      }
+      var j = (word.indexOf(guess));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+       
+      } else {
+        comments();
+      }
+    }
+  }
+  // Play
+  play = function () {
+    categories = [
+        ["babe ruth", "yogi berra", "mickey mantle", "joe dimaggio", "lou gehrig"],
+        ["derek jeter", "mariano rivera", "bernie williams", "don mattingly"],
+        ["joe torre", "billy martin", "casey stengel", "buck showalter"]
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    guesses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+   
+  }
+
+  play();
+  
+  // Hint
+
+    hint.onclick = function() {
+
+
+      hints = [
+        ["Was Traded from Boston", "He has 10 world series rings","Most homer by a switch hitter", "56 game hitting streak", "most consecutive games played by a Yankee"],
+        ["Won the rookie of the year in 1996", "All time leader in Saves", "AL Batting Champ in 1998", "Hit 400 in his 1st playoff appearance"],
+        ["Made 12 postseason appearance", "Won the 1977 World Series", "Won five consecutive World Series 1949–1953", "Manager during the 1995 strike"]
+      ];
+
+    var catagoryIndex = categories.indexOf(chosenCategory);
+    var hintIndex = chosenCategory.indexOf(word);
+     showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+      console.log("clue");  
+     };
+// Reset
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    play();
+  }
+}
+
 
 
 
